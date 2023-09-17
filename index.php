0b84499b3d7a172f38843257344353e519e7c7d8
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-17 19:43:11 -0400

    Uso de variables

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<?php
-echo 'Hola mundo';
+<?php 
+    $nombre = "Manuel Henriquez";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP 8</title>
+</head>
+<body>
+    <h1><?php echo "Hola mundo, mi nombre es $nombre" ?></h1>
+</body>
+</html>
