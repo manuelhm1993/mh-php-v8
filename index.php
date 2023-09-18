e799442e57b85cb95f1657141271ed5ab74d1a32
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-18 03:58:54 -0400

    strlen vs mb_strlen La primera retorna la cantidad de bits en una cadena La segunda retorna la cantidad de caracteres de una cadena Se recomienda la segunda ya que los caracteres con tílde ocupan dos bits

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php 
 
-declare(strict_types=1); // Declaración de tipos estricta
+$cadena = "aeiou";
 
-// Definir el tipo de datos a recibir y a devolver
-function sumarEnteros(int $valor1, int $valor2): int | float {
-    return ($valor1 + $valor2) / 2;
-}
-
-$resultado = sumarEnteros(2, 5);
-
-echo $resultado;
+// Esta es la función que se debe usar para determinar la cantidad de caracteres de una cadena
+echo mb_strlen($cadena); 
