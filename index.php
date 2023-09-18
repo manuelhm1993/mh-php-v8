2142c654e6e37aa12087ee723e8daa321230a548
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-18 03:09:11 -0400

    Ámbito local de las funciones, si no se declara una variable como global dentro de la función y esta tiene el mismo nombre que una variable fuera de la función, se tomará la del ámbito local

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 $a = 5; // Ámbito global
 
 function test() {
-    // $a; // Ámbito local - La variable $a del ámbito global no existe en la función
-    global $a; // Ámbito local - La variable $a es llamada del ámbito global y puede ser usada
+    $a = 4; // Ámbito local
 
     echo $a;
 }
