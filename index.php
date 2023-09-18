655b7ca59aae8f8abc9071e3bb0e3f6c0cd879fc
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-18 04:36:05 -0400

    Array multidimensionales asociativos

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 <?php 
 
-// Array asociativo
-$datos = [
-    'nombre'  => 'Manuel Henriquez',
-    'email'   => 'manuel@mhenriquez.com',
-    'celular' => '0424-1234567',
+// Array asociativo multidimensional
+$datos = [ // Dimensión 1
+    [ // Dimensión 2
+        'nombre'    => 'Manuel Henriquez',
+        'email'     => 'manuel@mhenriquez.com',
+        'celular'   => '0424-1234567',
+        'direccion' => [ // Dimensión 3
+            'pais'   => 'Venezuela',
+            'ciudad' => 'Maracaibo',
+        ],
+    ],
+    [
+        'nombre'  => 'Fanny Carvajal',
+        'email'   => 'fanny@mhenriquez.com',
+        'celular' => '0424-1234567',
+        'direccion' => [
+            'pais'   => 'Venezuela',
+            'ciudad' => 'Maracaibo',
+        ],
+    ],
+    [
+        'nombre'  => 'Julio Calderón',
+        'email'   => 'julio@mhenriquez.com',
+        'celular' => '0424-1234567',
+        'direccion' => [
+            'pais'   => 'Venezuela',
+            'ciudad' => 'Maracaibo',
+        ],
+    ],
 ];
 
-echo $datos['email'];
+// Para acceder a los datos se usan los índices de las dimensiones
+echo $datos[0]['direccion']['pais'];
