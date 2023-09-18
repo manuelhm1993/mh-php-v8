d8ef766b8190f2f9d4c12fa5e22322ea70bcc40e
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-18 02:50:07 -0400

    Funciones propias Se definió una función para calcular el factorial de un número dado Llamamiento de funciones

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php 
+// Funciones definidas por nosotros
+function factorial($value) {
+    $factorial = 1;
+    
+    for ($i = 1; $i <= $value; $i++) { 
+        $factorial *= $i;
+    }
 
-// Funciones predefinidas del lenguaje
-echo "Marca de tiempo: " . time();
-echo "<br>";
-echo "Raíz cuadrada de 144: " . sqrt(144);
-echo "<br>";
-echo "Número aleatorio entre 90-100: " . rand(90, 100);
-echo "<br>";
-echo "Número PI: " . pi();
-echo "<br>";
+    echo "El factorial de $value es: $factorial";
+    echo "<br>";
+}
+
+// Llamada de función
+factorial(0);
+factorial(9);
