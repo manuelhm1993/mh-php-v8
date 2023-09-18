f3bce5a0c3f10a8d187902db8436ae1379d80fc1
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-17 22:13:30 -0400

    Comprobar si una constante existe

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php 
 define('CURSO', 'PHP 8 desde cero'); // Forma clásica de definir constantes
 const USUARIO = 'MHenriquez'; // Esta es la forma de definir constantes de clase: private statis const y funciona fuera de una clase
-const ANIMALES = [ // Array como constante
+const ANIMALES = [ // Array como constante a partir de PHP 7
     'Perro',
     'Gato',
     'Loro',
     'Tortuga',
 ];
 
-echo "- Curso: " . CURSO 
-. "<br>"
-. "- Usuario: " . USUARIO
-. "<br>"
-. "- Animal: " . ANIMALES[0];
+// Comprobar si una constante está definida
+if(defined('USUARIO')) {
+    echo "El usuario está registrado";
+}
+else {
+    echo "El usuario NO está registrado";
+}
