ee4231bda243b524cd44f0c0e306657f42d43167
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-18 01:41:21 -0400

    Ejercicio del árbol de navidad Bucles for anidados

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php 
 
-// Ejercicio de potencia
-$base      = 10;
-$exponente = 2;
-$resultado = 1;
+// Ejercicio del árbol de navidad
+$filas = 10;
 
-// Para $i = 1 mientras sea <= $exponente hacer
-for ($i = 1; $i <= $exponente; $i++) { 
-    $resultado *= $base;
+for ($i = 1; $i <= $filas; $i++) { 
+    for ($j = 0; $j < $i; $j++) { 
+        echo "*";
+    }
+    echo "<br>";
 }
-
-echo "$base ^ $exponente = $resultado";
