cccfb905b97a5cd083bf36b7cd487d2fe1cf5f1b
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-18 00:35:25 -0400

    Operador ternario Permite hacer condiciones en una misma linea de código

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php 
 
-$a = 4;
+$a = 6;
 $b = 5;
+$message = "El valor de 'a' es menor que el valor de 'b'";
 
-// Estructura condicional anidada
-if($a < $b) {
-    echo "El valor de 'a' es menor que el valor de 'b'";
-}
-elseif($a > $b) {
-    echo "El valor de 'a' es mayor que el valor de 'b'";
-}
-else {
-    echo "El valor de 'a' es igual que el valor de 'b'";
-}
+// Operador ternario
+echo ($a < $b) 
+? $message 
+: (($a > $b) ? str_replace('menor', 'mayor' , $message) : str_replace('menor', 'igual' , $message));
