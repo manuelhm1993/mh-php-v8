95baec7aeca3df4a4ca1899fcd282cec41051777
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-18 04:48:59 -0400

    Desempaquetar un array en variables, se puede usar list o el destructory con spred operator

--- index.php
+++ index.php
@@ -1,43 +1,14 @@
 <?php 
 
-// Array asociativo multidimensional
-$datos = [ // Dimensión 1
-    [ // Dimensión 2
-        'nombre'    => 'Manuel Henriquez',
-        'email'     => 'manuel@mhenriquez.com',
-        'celular'   => '0424-1234567',
-        'direccion' => [ // Dimensión 3
-            'pais'   => 'Venezuela',
-            'ciudad' => 'Maracaibo',
-        ],
-    ],
-    [
-        'nombre'  => 'Fanny Carvajal',
-        'email'   => 'fanny@mhenriquez.com',
-        'celular' => '0424-1234567',
-        'direccion' => [
-            'pais'   => 'Venezuela',
-            'ciudad' => 'Maracaibo',
-        ],
-    ],
-    [
-        'nombre'  => 'Julio Calderón',
-        'email'   => 'julio@mhenriquez.com',
-        'celular' => '0424-1234567',
-        'direccion' => [
-            'pais'   => 'Venezuela',
-            'ciudad' => 'Maracaibo',
-        ],
-    ],
-];
+$array = [1, 2, 3];
 
-// Iterar todos los datos del array
-foreach($datos as $usuario) {
-    echo "- Nombre: {$usuario['nombre']}"; // Interpolación compleja
-    echo "<br>";
-    echo "- Correo: {$usuario['email']}"; // Interpolación compleja
-    echo "<br>";
-    echo "- Celular: {$usuario['celular']}"; // Interpolación compleja
-    echo "<br>";
-    echo "<hr>";
-}
+// Desempaquetar un array en variables
+//[$a, $b, $c] = [...$array]; // Destructory
+list($a, $b, $c) = $array;
+
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo $c;
+echo "<br>";
