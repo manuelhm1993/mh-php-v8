3225319a52cf0469ac0177e010a8fc591be50b72
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-18 00:21:41 -0400

    Condicionales Permiten manipular el flujo de ejecución si se cumple o no una determinada condición

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php 
-$a = "Manuel";
-$b = null;
 
-var_dump($a && $b);
+$a = 5;
+$b = 3;
+
+echo "Primer párrafo";
+echo "<br>";
+
+// Estructura condicional
+if($a < $b) {
+    echo "Segundo párrafo";
+    echo "<br>";
+}
+else {
+    echo "Segundo párrafo editado";
+    echo "<br>";
+}
+
+echo "Tercer párrafo";
+echo "<br>";
