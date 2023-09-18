854259e714851455a612ca5719eb04ab4ffacd4c
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-18 03:51:41 -0400

    Especificar varios tipos de datos a devolver

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 declare(strict_types=1); // Declaración de tipos estricta
 
 // Definir el tipo de datos a recibir y a devolver
-function sumarEnteros(int $valor1, int $valor2): int {
-    return $valor1 + $valor2;
+function sumarEnteros(int $valor1, int $valor2): int | float {
+    return ($valor1 + $valor2) / 2;
 }
 
 $resultado = sumarEnteros(2, 5);
