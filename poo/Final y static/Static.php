3faa75eddf4e0c4e616334b20d0a2fd81e50d89f
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-19 03:20:15 -0400

    Las propiedades también pueden ser static, pero para guardar la encapsulación, se deben usar métodos accesores

--- poo/Final y static/Static.php
+++ poo/Final y static/Static.php
@@ -1,7 +1,13 @@
 <?php
 
 class Humano {
+    private static string $nombre = "Manuel Henriquez";
+
     public static function saludar(): void {
         echo "Hola mundo";
+    }
+
+    public static function getNombre(): string {
+        return self::$nombre;
     }
 }
--- poo/Final y static/index.php
+++ poo/Final y static/index.php
@@ -2,4 +2,6 @@
 
 require_once "Static.php";
 
-Humano::saludar();
+// Humano::saludar(); // Ejecutar un método static sin ningún objeto
+
+echo Humano::getNombre(); // Obtener el valor de una propiedad static
