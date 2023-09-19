69e9f8107b51259b3cec711f71224d49de8777af
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-19 01:54:10 -0400

    Llamado de clases dentro de un namespace

--- poo/Namespaces/Carpeta1/Humano.php
+++ poo/Namespaces/Carpeta1/Humano.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Namespaces\Carpeta1; // Declaración de namespace, debe ser la primera instrucción luego de la etiquetat php
+
 class Humano {
     public function saludar() {
         echo "Hola mundo desde la clase Humano dentro de la Carpeta1";
--- poo/index.php
+++ poo/index.php
@@ -1,5 +1,10 @@
 <?php
 
+use Namespaces\Carpeta2\Humano;
+
 require_once "Namespaces/Carpeta1/Humano.php";
 require_once "Namespaces/Carpeta2/Humano.php";
 
+$humano1 = new Humano;
+
+$humano1->saludar();
