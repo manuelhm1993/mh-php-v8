d38b6e3929972db4a7556a5066e9f8a7957225c1
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-19 01:35:56 -0400

    Anotaciones de traits

--- poo/Clases/Herencia/Traits/LatinoAmericano.php
+++ poo/Clases/Herencia/Traits/LatinoAmericano.php
@@ -2,6 +2,7 @@
 
 // Los traits son un mecanispo para suplir la herencia simple de php y agregan comportamientos extra a las clases
 trait LatinoAmericano {
+    // Al igual que las clases abstractas y las interfaces, los trais no se instancian, sirven para extender en funcionalidad una clase
     public function saludoLatinoAmericano(): void {
         echo "Este es un saludo latino americano";
     }
