6ca0d7c89fe3453d1e977f9c347bb23e7fdaf01a
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-19 02:00:45 -0400

    Comentarios de instrucciones para usar los namespaces

--- poo/index.php
+++ poo/index.php
@@ -1,10 +1,19 @@
 <?php
 
-use Namespaces\Carpeta2\Humano;
+use Namespaces\Carpeta1\Humano;
+use Namespaces\Carpeta2\Humano as Humano2; // Importar las clases dentro del namespace y alias
 
 require_once "Namespaces/Carpeta1/Humano.php";
-require_once "Namespaces/Carpeta2/Humano.php";
+require_once "Namespaces/Carpeta2/Humano.php"; // Requerir el archivo que incluye el namespace
 
-$humano1 = new Humano;
+// Colección de humanos
+$humanos = [
+    'manuel'  => new Humano,
+    'desiree' => new Humano2,
+];
 
-$humano1->saludar();
+// Iterar con foreach
+foreach ($humanos as $humano) {
+    $humano->saludar();
+    echo "<br>";
+}
