b2843adc7bcc70d9791d7588795c60c40f244447
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-19 04:18:55 -0400

    Atributos con nombre PHP 8, permiten enviar los argumentos de la función en cualquier orden y se asignan correctamente

--- poo/Fluent interface y atributos con nombre/index.php
+++ poo/Fluent interface y atributos con nombre/index.php
@@ -2,6 +2,7 @@
 
 require_once "Humano.php";
 
-$humano = new Humano('Manuel', 'Henriquez', 'Moreno', '123456789');
+// Parámetros con nombre PHP 8, hace algo similar a los arrays asociativos, permite enviar argumentos en cualquier orden
+$humano = new Humano(dni:'123456789' , apellidoPaterno:'Henriquez', nombre:'Manuel', apellidoMaterno:'Moreno');
 
 $humano->imprimirDatos();
