1e9fc802462ea1954288d71e10aada55b935691e
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-24 03:59:52 -0400

    Directorio de rutas definito El index.php se hace responsable solo de hacer importaciones y configuraciones

--- mvc/public/index.php
+++ mvc/public/index.php
@@ -1,17 +1,4 @@
 <?php
 
 require_once '../autoload.php';
-
-use Lib\Route;
-
-Route::get('/', function () {
-    echo "Hola desde la página principal";
-});
-
-Route::get('/contact', function () {
-    echo "Hola desde la página de contacto";
-});
-
-Route::get('/about', function () {
-    echo "Hola desde la página acerca de";
-});
+require_once '../routes/web.php';
---
+++ mvc/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Lib\Route;
+
+Route::get('/', function () {
+    echo "Hola desde la página principal";
+});
+
+Route::get('/contact', function () {
+    echo "Hola desde la página de contacto";
+});
+
+Route::get('/about', function () {
+    echo "Hola desde la página acerca de";
+});
