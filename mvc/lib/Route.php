2b538a779c03f14737d7d7eabe52aeca8037f378
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-24 03:22:59 -0400

    Registrar rutas de tipo get y de tipo post a través de métodos static Se le pasa la ruta como uri y un callback para la acción

--- mvc/lib/Route.php
+++ mvc/lib/Route.php
@@ -4,17 +4,15 @@ namespace Lib;
 
 class Route {
     // Propiedad para almacenar las URLs válidas de la aplicación
-    private array $routes = [
-        'GET' => [
-            'about'   => function () {
+    private static array $routes = [];
 
-            },
-            'contact' => function () {
+    // Registra las rutas de tipo GET
+    public static function get(string $uri, callable $callback): void {
+        self::$routes['GET'][$uri] = $callback;
+    }
 
-            },
-        ],
-        'POST' => [
-            
-        ],
-    ];
+    // Registra las rutas de tipo POST
+    public static function post(string $uri, callable $callback): void {
+        self::$routes['POST'][$uri] = $callback;
+    }
 }
