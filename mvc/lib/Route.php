1f11516d6d7f180451767f7f24d46950e39d9bb9
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-24 03:08:12 -0400

    Clase Route Esta clase definirá el sistema de routing a través de las urls válidas de la app

---
+++ mvc/lib/Route.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Lib;
+
+class Route {
+    // Propiedad para almacenar las URLs válidas de la aplicación
+    private array $routes = [
+        'about'   => function () {
+
+        },
+        'contact' => function () {
+
+        },
+    ];
+}
