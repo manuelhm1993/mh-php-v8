bd9f428fd27b6dc266e73ef4aa521d779ddc31d9
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-24 06:13:37 -0400

    Limpiar comentarios innecesarios

--- mvc/lib/Route.php
+++ mvc/lib/Route.php
@@ -34,26 +34,23 @@ class Route {
     }
 
     public static function dispatch(): void {
-        $uri    = $_SERVER["REQUEST_URI"]; // Recuperar la url de la petición actual
+        $uri    = $_SERVER["REQUEST_URI"];    // Recuperar la url de la petición actual
         $method = $_SERVER["REQUEST_METHOD"]; // Rescatar el verbo http de la petición
 
         $uri = self::clearUri($uri);
 
         // Iterar las rutas registradas en el verbo http de la petición
         foreach (self::$routes[$method] as $route => $callback) {
-            // Verificar si existe el caracter ':' y sobreescribir la uri
             if (strpos($route, ':') !== false) {
                 // El subpatrón devuelve la coincidencia a la variable matches
                 $route = preg_replace('#:[a-z0-9]+#i', '([a-zA-Z0-9]+)', $route);
             }
 
             // Si la url de la petición coincide con una ruta, se ejecuta su callback y sale de la función
-            // Se pude usar los delimitadores '//' o '##' para las expresiones regulares
             if (preg_match("#^$route$#", $uri, $matches)) {
                 // Obtener las coincidencias del subpatrón sin contar la url, solo los parámetros
                 $params = array_slice($matches, 1);
 
-                // Desempaquetar el array en distintas variables (spread operator)
                 $response = $callback(...$params);
 
                 // Si la respuesta es un array o un objeto se devuelve codificado para no dar error al imprimir
@@ -69,7 +66,6 @@ class Route {
             }
         }
 
-        // Si la url de la petición no es encontrada, se emite un mensaje de error;
         echo "404 | La ruta $uri no existe";
     }
 }
