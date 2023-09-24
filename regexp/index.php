3cf53b3253be71302c0e4e14d1bc0323a62520ec
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-24 06:35:36 -0400

    Subpatrones de captura Permiten capturar los valores coincidentes dentro del array matches

--- regexp/index.php
+++ regexp/index.php
@@ -2,7 +2,12 @@
 
 $cadena = "Llegaré pronto que voy andando";
 
-// Subpatrones
+// Subpatrones de captura
 $expresion = "/Llegaré pronto que voy (vol|and)ando/i";
 
-echo (preg_match($expresion, $cadena)) ? 'Cumple' : 'No cumple';
+// El subpatrón que coincide es enviado a un array llamado matches que se pasa como tercer parámetro de preg_match
+echo (preg_match($expresion, $cadena, $matches)) ? 'Cumple' : 'No cumple';
+
+// En la primera posición queda almacenada la cadena y en el siguiente el subpatrón coincidente, en este caso (and)
+echo "<br>";
+var_dump($matches);
