629a7ba529179017fade406d56f7be04a6f77417
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-24 07:12:52 -0400

    Definir subpatrones que no devuelvan valor a capturar ?:

--- regexp/index.php
+++ regexp/index.php
@@ -1,14 +1,14 @@
 <?php
 
-$url = 'youtu.be/xGaXH7spCyA?si=ttFBCojtll02odCd';
-// $url = 'https://www.youtube.com/watch?v=xGaXH7spCyA&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=40';
+// $url = 'youtu.be/xGaXH7spCyA?si=ttFBCojtll02odCd';
+$url = 'https://www.youtube.com/watch?v=xGaXH7spCyA&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=40';
 
 // Usar los '#' como delimitadores ya que se está trabajando con http:// y habría que escaparlos
-$pattern = "#^(htt(p|ps)://)?(www\.)?(youtu\.be/|youtube\.com/watch\?v=)(\w+)#";
+$pattern = "#^(?:htt(?:p|ps)://)?(?:www\.)?(?:youtu\.be/|youtube\.com/watch\?v=)(\w+)#i";
 
 // Recuperar los subpatrones con matches
 if (preg_match($pattern, $url, $matches)) {
-    echo "La url es válida: id - {$matches[5]}";
+    echo "La url es válida: id - {$matches[1]}";
     // var_dump($matches);
 } 
 else {
