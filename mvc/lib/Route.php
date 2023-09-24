bded0943312affed254fd0c2350514780d163c93
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-24 04:27:17 -0400

    Recuperar la url, limpiarla de caracteres y espacios con trim y el verbo http de la petición

--- mvc/lib/Route.php
+++ mvc/lib/Route.php
@@ -3,22 +3,42 @@
 namespace Lib;
 
 class Route {
-    // Propiedad para almacenar las URLs válidas de la aplicación
+    /**
+     * Propiedad para almacenar las URLs válidas de la aplicación
+     */
     private static array $routes = [];
 
-    // Registra las rutas de tipo GET
+    /**
+     * Limpia las url quitando espacios y el simbolo '/'
+     */
+    private static function clearUri(string $uri): string {
+        return trim($uri, '/');
+    }
+
+    /**
+     * Registra las rutas de tipo GET
+     */
     public static function get(string $uri, callable $callback): void {
+        self::clearUri($uri);
         self::$routes['GET'][$uri] = $callback;
     }
 
-    // Registra las rutas de tipo POST
+    /**
+     * Registra las rutas de tipo POST
+     */
     public static function post(string $uri, callable $callback): void {
+        self::clearUri($uri);
         self::$routes['POST'][$uri] = $callback;
     }
 
     public static function dispatch(): void {
-        $uri = $_SERVER["REQUEST_URI"];
+        $uri    = $_SERVER["REQUEST_URI"]; // Recuperar la url de la petición actual
+        $method = $_SERVER["REQUEST_METHOD"]; // Rescatar el verbo http de la petición
+
+        $uri = self::clearUri($uri);
 
         echo $uri;
+        echo "<br>";
+        echo $method;
     }
 }
